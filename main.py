7b09494e8c90c3da96ed8e157cc5024f8e27d4e5
Shehab Yasser <shehabyasser@gmail.com>
2023-06-28 01:12:39 -0400

    Added a football example

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from datasets import load_dataset
 if __name__ == '__main__':
     dataset = load_dataset("cnn_dailymail", version="3.0.0")
     print(dataset)
-    print(f"Features: {dataset['train'].column_names}")
+
+    sample = dataset["train"][23]
+    print(sample)
